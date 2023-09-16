9de1c912b2ea261be461b7080c2d8670ef242f04
Cactus <neel0108p@gmail.com>
2023-09-16 09:43:33 -0600

    Time: 0 ms (100%), Space: 38.7 MB (95.32%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] allowedSteps = new int[] { 1, 2};
+        return find(n, allowedSteps, new HashMap<Integer, Integer>());
+    }
+
+    int find(int n, int[] allowedSteps, Map<Integer, Integer> memo){
+        int count =0;
+        if(memo.containsKey(n))
+            return memo.get(n);
+        if (n < 0)
+            return 0;    
+        if(n == 0)
+            return 1;
+        for(int i = 0; i < allowedSteps.length; i++){
+            int res = find(n - allowedSteps[i], allowedSteps, memo);
+            if(res>0){
+                count += res;
+            }
+        }
+        memo.put(n, count);
+        return count;
+    }
+}
