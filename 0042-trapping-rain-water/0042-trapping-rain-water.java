e416cdda1735deb7b6341782cdbdf47e745b8205
Cactus <neel0108p@gmail.com>
2023-09-17 12:35:23 -0600

    Time: 1 ms (79.33%), Space: 44 MB (78.53%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,28 +1,26 @@
 class Solution {
+    /* 
+    * Time Complexity: O(N)
+    * Space Complexity: O(N); Extra Space
+    */
     public int trap(int[] height) {
-        int maxL = height[0];
-        int maxR = height[height.length-1];
-        int l = 0;
-        int r = height.length-1;
         int trappedWater = 0;
-         while (l < r) {
-            if (maxL <= maxR) {
-                l++;
-                if (height[l] < maxL) {
-                    trappedWater += maxL - height[l];
-                } else {
-                    maxL = height[l];
-                }
-            } else {
-                r--;
-                if (height[r] < maxR) {
-                    trappedWater += maxR - height[r];
-                } else {
-                    maxR = height[r];
-                }
-            }
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+        int[] minLR = new int[height.length];
+        leftMax[0] = height[0];
+        rightMax[height.length-1] = height[height.length-1];
+        for(int i=1, j=height.length-2; i<height.length; i++, j--){
+            leftMax[i] = Math.max(leftMax[i-1], height[i]);
+            rightMax[j] = Math.max(rightMax[j+1], height[j]);
         }
-
+        for(int i=0; i<height.length; i++){
+            minLR[i] = Math.min(leftMax[i], rightMax[i]);
+        }
+        for(int i=0; i<height.length; i++){
+            if(height[i] <= minLR[i])
+                trappedWater +=  minLR[i] - height[i];
+        }
         return trappedWater;
     }
 }
