29ac5504e899fa35bcb2d38b61f7a88d74f43bde
Cactus <neel0108p@gmail.com>
2023-09-18 22:05:05 -0600

    Time: 2 ms (37.95%), Space: 58.8 MB (69.4%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,16 +1,16 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         cyclicSort(nums);
-        for(int i = 0; i<nums.length; i++){
+        for(int i=0; i<nums.length; i++)
             if(nums[i] != i+1)
                 return i+1;
-        }
         return nums.length+1;
     }
+
     void cyclicSort(int[] nums){
         int i = 0;
         while(i<nums.length){
-            if(nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[nums[i]-1])
+            if(nums[i]>0 && nums[i]<nums.length && nums[i] != nums[nums[i]-1])
                 swap(nums, i, nums[i]-1);
             else
                 i++;
