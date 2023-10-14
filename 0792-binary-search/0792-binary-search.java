246f93eba31150f0af0d0cd7d3dd436faa84c186
Cactus <neel0108p@gmail.com>
2023-10-14 02:13:13 -0600

    Time: 0 ms (100%), Space: 44.3 MB (83.59%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -2,8 +2,7 @@ class Solution {
     public int search(int[] nums, int target) {
         if(nums.length == 0)
             return -1;
-        if(nums.length == 1)
-            return nums[0] == target ? 0 : -1;
+        
         int s = 0;
         int e = nums.length-1;
         while(s<=e){
