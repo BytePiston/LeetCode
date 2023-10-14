dec73788eb34ee639720c687e2372ef1ac563fa8
Cactus <neel0108p@gmail.com>
2023-10-14 02:00:16 -0600

    Time: 4 ms (74.47%), Space: 41.5 MB (98.61%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,14 +2,19 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length())
             return false;
-        int[] result = new int[26];
-        for(int i=0; i  < s.length(); i++)
-            result[s.charAt(i)-'a']++;
-        for(int i=0; i< t.length(); i++)
-            result[t.charAt(i)-'a']--;
-        for(int i=0; i < result.length; i++)
-            if(result[i] != 0)
+        int[] arr = new int[26];
+        for(int i=0; i<s.length(); i++){
+            arr[s.charAt(i)-'a']++;
+        }
+
+        for(int i=0; i<t.length(); i++){
+            arr[t.charAt(i)-'a']--;
+        }
+
+        for(int i: arr){
+            if(i!=0)
                 return false;
-        return true;
+        }
+        return true; 
     }
 }
