c7d292e158c58dd5aa529ce95c4b113d3296dde0
Cactus <neel0108p@gmail.com>
2023-10-22 05:25:41 -0600

    Time: 1 ms (99.19%), Space: 44.2 MB (12.96%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,38 +1,30 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> res = new LinkedList<>();
+        List<int[]> result = new LinkedList<>();
         int newStart = newInterval[0];
         int newEnd = newInterval[1];
         int index = 0;
-
-        // Add all intervals that end before newInterval starts
-        while (index < intervals.length && intervals[index][1] < newStart) {
-            res.add(intervals[index]);
-            index++;
+        //newStart < intervals[i][1]
+        while(index<intervals.length && intervals[index][1] < newStart){
+            result.add(intervals[index++]);
         }
 
-        // Merge intervals that overlap with newInterval
-        while (index < intervals.length && intervals[index][0] <= newEnd) {
+        //newEnd <= intervals[i][0]
+        while(index<intervals.length && intervals[index][0] <= newEnd){
             newStart = Math.min(newStart, intervals[index][0]);
             newEnd = Math.max(newEnd, intervals[index][1]);
             index++;
         }
+        result.add(new int[]{newStart, newEnd});
 
-        // Add the merged interval
-        res.add(new int[]{newStart, newEnd});
-
-        // Add the remaining intervals
-        while (index < intervals.length) {
-            res.add(intervals[index]);
-            index++;
+        while(index<intervals.length){
+            result.add(intervals[index++]);
         }
 
-        // Convert the list to a 2D array
-        int[][] result = new int[res.size()][2];
-        for (int i = 0; i < res.size(); i++) {
-            result[i] = res.get(i);
+        int[][] output = new int[result.size()][2];
+        for(int i = 0; i<result.size(); i++){
+            output[i] = result.get(i);
         }
-
-        return result;
+        return output;
     }
 }
