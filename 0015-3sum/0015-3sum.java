d2edae35249893bc721edb5a9678158fd945b009
Cactus <neel0108p@gmail.com>
2023-10-23 05:46:35 -0600

    Time: 31 ms (67.04%), Space: 51.2 MB (30.07%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -8,23 +8,20 @@ class Solution {
             int s = i+1;
             int e = nums.length-1;
             while(s<e){
-                
-                    int sum = nums[i] + nums[s] + nums[e];
-                    if(sum == 0){
-                        result.add(Arrays.asList(nums[i], nums[s], nums[e]));  
-                        while(s<e && nums[s] == nums[s+1]){
-                            s++;
-                        }
-                        while(s<e && nums[e] == nums[e-1]){
-                            e--;
-                        }
+                int sum = nums[i] + nums[s] + nums[e];
+                if(sum == 0){
+                    result.add(Arrays.asList(nums[i], nums[s], nums[e]));
+                    while(s<e && nums[s] == nums[s+1])
                         s++;
+                    while(s<e && nums[e] == nums[e-1])
                         e--;
-                    } else if(sum < 0){
-                        s++;
-                    }else{
-                        e--;
-                    }              
+                    s++;
+                    e--;
+                }else if(sum < 0){
+                    s++;
+                }else{
+                    e--;
+                }
             }
         }
         return result;
