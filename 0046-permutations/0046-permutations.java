a336e415491fbdae999613d2cbf181964a901807
Cactus <neel0108p@gmail.com>
2023-12-11 00:35:49 -0700

    Time: 4 ms (6.98%), Space: 43.6 MB (72.51%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,4 +1,6 @@
 class Solution {
+    
+/*  //Using Extra Space for memo to remeber visited indexes and backtrack;    
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         permuteHelper(result, new ArrayList<>(), new boolean[nums.length], nums);
@@ -21,5 +23,32 @@ class Solution {
                 memo[i] = false;
             }
         }
+    }
+*/
+    
+    //Using NO Extra Space; We will swap elements in the array to create new permutaions;
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        permuteHelper(0, result, nums);
+        return result;
+    }
+    
+    void permuteHelper(int index, List<List<Integer>> result, int[] nums){
+        if(index == nums.length){
+            List<Integer> answer = Arrays.stream(nums).boxed().collect(Collectors.toList());
+            result.add(answer);
+        }
+        
+        for(int i=index; i<nums.length; i++){
+            swap(nums, index, i);
+            permuteHelper(index+1, result, nums);
+            swap(nums, index, i);
+        }
+    }
+    
+    void swap(int[] nums, int left, int right){
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
