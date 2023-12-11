491c5aab8ac3ed17c9011971c13dd3c73f992deb
Cactus <neel0108p@gmail.com>
2023-12-11 00:09:52 -0700

    Time: 1 ms (99.33%), Space: 44 MB (37.90%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,30 +1,27 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> result = new LinkedList<>();
-        int newStart = newInterval[0];
+        List<int[]> result = new ArrayList<>();
+        int newBegin = newInterval[0];
         int newEnd = newInterval[1];
         int index = 0;
-        //newStart < intervals[i][1]
-        while(index<intervals.length && intervals[index][1] < newStart){
+        
+        //Find all the intervals for which our end is less than equal to newBegin;
+        while(index<intervals.length && intervals[index][1]<newBegin){
             result.add(intervals[index++]);
         }
-
-        //newEnd <= intervals[i][0]
-        while(index<intervals.length && intervals[index][0] <= newEnd){
-            newStart = Math.min(newStart, intervals[index][0]);
+        //Find all the intervals for which our start is less than equal to newEnd;
+        while(index<intervals.length && intervals[index][0]<=newEnd){
+            newBegin = Math.min(newBegin, intervals[index][0]);
             newEnd = Math.max(newEnd, intervals[index][1]);
             index++;
         }
-        result.add(new int[]{newStart, newEnd});
-
+        
+        result.add(new int[]{newBegin, newEnd});
+        
+        //Add remining intervals which are not affected.
         while(index<intervals.length){
             result.add(intervals[index++]);
         }
-
-        int[][] output = new int[result.size()][2];
-        for(int i = 0; i<result.size(); i++){
-            output[i] = result.get(i);
-        }
-        return output;
+        return result.toArray(new int[result.size()][]);
     }
 }
