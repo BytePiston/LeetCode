5549e1fdef0a99cf882952cc122821919dfa3d96
Cactus <neel0108p@gmail.com>
2023-12-11 00:57:18 -0700

    Time: 3 ms (61.20%), Space: 44.2 MB (24.48%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,56 +1,66 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
+        if(n<1){
+            return new ArrayList<>();
+        }
+        List<List<String>> result = new ArrayList<>();
         boolean[][] board = new boolean[n][n];
-        List<List<String>> outer = new ArrayList<>();
-        solve(board, outer, 0);
-        return outer;
+        solveNQueensHelper(result, 0, board);
+        return result;
     }
-
-    void solve(boolean[][] board, List<List<String>>outer, int row){
+    
+    void solveNQueensHelper(List<List<String>> result, int row, boolean[][] board){
         if(row == board.length){
-            outer.add(construct(board));
+            result.add(constructBoard(board));
             return;
         }
-        for(int col=0; col<board[0].length; col++){
-            if(isSafe(board, row, col)){
+        
+        for(int col=0; col<board[row].length; col++){
+            if(isSafeToPlace(board, row, col)){
                 board[row][col] = true;
-                solve(board, outer, row+1);
+                solveNQueensHelper(result, row+1, board);
                 board[row][col] = false;
             }
         }
     }
 
-    boolean isSafe(boolean[][] board, int row, int col){
-        //check for Vertical
-        for(int i=0; i<row; i++)
-            if(board[i][col])
+    boolean isSafeToPlace(boolean[][] board, int row, int col){
+        //Validate vertical
+        for(int i=0; i<row; i++){
+            if(board[i][col]){
                 return false;
-
-        //check for left diagonal
-        for(int i=1; i<=Math.min(row, col); i++)
-            if(board[row-i][col-i])
-                return false;
+            }
+        }
         
-        //check for right diagonal
-        for(int i=1; i<=Math.min(row, board.length-col-1); i++)
-            if(board[row-i][col+i])
+        //Validate left diagnal
+        for(int i=1; i<=Math.min(row, col); i++){
+            if(board[row-i][col-i]){
                 return false;
-
+            }
+        }
+        
+        //Validate right diagnal
+        for(int i=1; i<=Math.min(row, board.length-col-1); i++){
+            if(board[row-i][col+i]){
+                return false;
+            }
+        }
         return true;
     }
-
-    List<String> construct(boolean[][] board){
-        List<String> result = new LinkedList<>();
-        for(int i=0; i<board.length; i++){
+    
+    List<String> constructBoard(boolean[][] board){
+        List<String> constructedBoard = new ArrayList<>();
+        for(int row=0; row<board.length; row++){
             StringBuilder sb = new StringBuilder();
-            for(int j=0; j<board.length; j++){
-                if(board[i][j])
+            for(int col=0; col<board[row].length; col++){
+                if(board[row][col]){
                     sb.append("Q");
-                else
+                } else{
                     sb.append(".");
+                }
             }
-            result.add(sb.toString());
+            constructedBoard.add(sb.toString());
         }
-        return result;
+        return constructedBoard;
     }
 }
