8b962a9476193d6b88b4b5cbfa1760fad284d0fe
Cactus <neel0108p@gmail.com>
2024-01-10 00:50:13 -0700

    Time: 8 ms (16.12%), Space: 42.1 MB (5.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,20 +1,21 @@
 class Solution {
-    int[][] tab;
     public int uniquePaths(int m, int n) {
-        tab = new int[m][n];
-        for (int[] row: tab)
-            Arrays.fill(row, -1);
-        return solve(m-1, n-1);
+        return solve(m-1, n-1, new HashMap<>());
     }
 
-    int solve(int m, int n){
-        if(m<0 || n<0)
+    int solve(int m, int n, Map<String, Integer> memo){
+        String key = m+" "+n;
+        if(memo.containsKey(key)){
+            return memo.get(key);
+        }
+        if(m < 0 || n < 0){
             return 0;
-        else if(m==0 && n==0)
+        }
+        if(m == 0 && n == 0){
             return 1;
-        if(tab[m][n] != -1)
-            return tab[m][n];
-        tab[m][n] =  solve(m-1,n) + solve(m,n-1);
-        return tab[m][n];
+        }
+        int sum = solve(m-1, n, memo) + solve(m, n-1, memo);
+        memo.put(key, sum);
+        return memo.get(key);
     }
 }
