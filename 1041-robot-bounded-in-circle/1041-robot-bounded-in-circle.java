f4543fe5a8ac386f86a64da7c64c6a9fb3e2b527
Cactus <neel0108p@gmail.com>
2024-01-10 17:58:33 -0700

    Time: 0 ms (100.00%), Space: 41.3 MB (25.94%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
@@ -1,4 +1,44 @@
 class Solution {
+    
+    //dir int folllows anticlockwise rotation as follows:
+    // 0 -> 'N'
+    // 1 -> 'W'
+    // 2 -> 'S'
+    // 3 -> 'E'
+    
+    //Optimised Approach with less if-else conditions
+    public boolean isRobotBounded(String instructions) {
+        
+        //Initially pos. (0, 0);
+        int[] coordinates = {0, 0};
+        
+        //Initially keeping dir as 0 -> 'N'
+        int dir = 0;
+        
+        //2D Array keeping track of all direction change in X and Y coordinates change N, W, S, E respectively;
+        int[][] dirArray = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
+        
+        //Iterating though instructions and processing each char;
+        for(char ch: instructions.toCharArray()){
+            if(ch == 'L'){
+                dir = (dir+1) % 4;
+            } else if(ch == 'R'){
+                dir = (dir+3) % 4;
+            } else{
+                coordinates[0] += dirArray[dir][0];
+                coordinates[1] += dirArray[dir][1];
+            }
+            
+        }
+        if(coordinates[0] == 0 && coordinates[1] == 0)
+            return true;   
+        if (dir == 0)
+            return false;
+        return true;
+    }
+    
+/*
+    //Native Approach
     public boolean isRobotBounded(String instructions) {
         int xCoordinate = 0;
         int yCoordinate = 0;
@@ -45,4 +85,5 @@ class Solution {
             return false;
         return true;
     }
+*/
 }
