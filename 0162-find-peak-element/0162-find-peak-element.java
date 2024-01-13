bf84a7cfe3f194d4c046230e61b7ff9d22e041a5
Cactus <neel0108p@gmail.com>
2024-01-12 19:56:22 -0700

    Time: 0 ms (100.00%), Space: 42.6 MB (11.16%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,6 +1,22 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-       return findPeakNative(nums);
+    //  return findPeakNative(nums);    //  O(N) time complexity;
+        return findPeakBinarySearch(nums);   //  O(log N) time complexity;
+    }
+    
+    //  Using Binary search technique to indentify Peak elements from given Array;
+    int findPeakBinarySearch(int[] nums){
+        int low = 0;
+        int high = nums.length-1;
+        while(low<high){
+            int mid = low + (high-low)/2;   //  To find mid element to avoid out of bound issue for large array size;
+            if(nums[mid] > nums[mid+1]){    // If mid is greater than next element meaning we have come to far adjust high;
+                high = mid;
+            }   else if(nums[mid] < nums[mid+1]){    // If mid is lesser than next element meaning we have come to far adjust high;
+                low = mid +1;
+            }
+        }
+        return low;
     }
     
     //  Iterative Native Solution where peak var is first set to 0 and for each element in the array if we find any element greater or equal to element at peak index;
