37766e54149c979335b6e637d5fc4b911a499edd
Cactus <neel0108p@gmail.com>
2024-01-12 17:00:44 -0700

    Time: 1 ms (88.55%), Space: 43.4 MB (41.99%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,19 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        solve(result, n, 0, "");
+        generateParenthesisHelper(result, n, 0, "");
         return result;
     }
 
-    void solve(List<String>result, int lp, int rp, String ps){
-        if(lp == 0 && rp == 0){
-            result.add(ps);
+    void generateParenthesisHelper(List<String> result, int leftOpeningCount, int rightClosingCount, String processed){
+        
+        //  Check if both Left and Right Parenthesis combination are genereted, if so add it to Result List;
+        if(leftOpeningCount == 0 && rightClosingCount == 0){
+            result.add(processed);
             return;
         }
-
-        if(lp > 0)
-            solve(result, lp-1, rp+1, ps + "(");
-        if(rp > 0)
-            solve(result, lp, rp-1, ps + ")");
+        
+        //  If both Left Parenthesis count is GT 0 then add it to processed String reduce left count and increase right count for generating the matching closing parenthesis;
+        if(leftOpeningCount > 0){
+            generateParenthesisHelper(result, leftOpeningCount-1, rightClosingCount+1, processed+"(");
+        }
+        
+        //  If both Right Parenthesis count is GT 0 then add it to processed String keep left count as it is decrease right count for generating the matching closing parenthesis;
+        if(rightClosingCount > 0){
+            generateParenthesisHelper(result, leftOpeningCount, rightClosingCount-1, processed+")");
+        }
     }
 }
