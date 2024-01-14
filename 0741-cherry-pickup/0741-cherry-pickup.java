ee654c8b26953245206054c3ef4de9b05eacbee6
Cactus <neel0108p@gmail.com>
2024-01-13 23:53:30 -0700

    Time: 11 ms (91.29%), Space: 44.4 MB (65.80%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -1,18 +1,19 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
-        int gridSize = grid.length;
-        int[][][] dp = new int[gridSize][gridSize][gridSize];
+        int[][][] dp = new int[grid.length][grid.length][grid.length];
         int maxCherriesPicked = solveRecursion(grid, 0, 0, 0, dp);
-        return  maxCherriesPicked < 0 ? 0 : maxCherriesPicked;
+        return maxCherriesPicked < 0 ? 0 : maxCherriesPicked;
     }
     
     int solveRecursion(int[][] grid, int r1, int c1, int c2, int[][][] dp){
-        
-        // Using Manhatten Distance formula calculate r2 value;
         int r2 = r1 + c1 - c2;
         int gridSize = grid.length-1;
         
-        if(r1>gridSize || r2>gridSize || c1 >gridSize || c2>gridSize || grid[r1][c1] == -1 || grid[r2][c2] == -1){
+        if(r1 == gridSize && c1 == gridSize){
+            return grid[r1][c1];
+        }
+        
+        if(r1>gridSize || r2>gridSize || c1>gridSize || c2>gridSize || grid[r1][c1] == -1 || grid[r2][c2] == -1){
             return Integer.MIN_VALUE;
         }
         
@@ -20,26 +21,17 @@ class Solution {
             return dp[r1][c1][c2];
         }
         
-        if(r1 == gridSize && c1 == gridSize){
-            return grid[r1][c1];
-        }
-        int maxCherriesPicked = 0;
+        int maxCherriesPicked = grid[r1][c1];
         
-        //  Only adding values from grid[r1][c1];
-        maxCherriesPicked = grid[r1][c1];
-        
-        // If grid[r1][c1] != grid[r2][c2]; i.e: both not at same cell position then add both else pick one;
         if(r1 != r2 && c1 != c2){
             maxCherriesPicked += grid[r2][c2];
         }
         
-        //  Manhatten Distance formula will adjust the of missing value;
-        //  i.e: for "downdown" only r1 change is provided but for "rightdown" formula will adjust r2 to r2+1 to balance c1+1; 
-        int downdown = solveRecursion(grid, r1+1, c1, c2, dp);
-        int downright = solveRecursion(grid, r1+1, c1, c2+1, dp);
-        int rightdown = solveRecursion(grid, r1, c1+1, c2, dp);
-        int rightright = solveRecursion(grid, r1, c1+1, c2+1, dp);
+        int downDownDir = solveRecursion(grid, r1+1, c1, c2, dp);
+        int downRightDir = solveRecursion(grid, r1+1, c1, c2+1, dp);
+        int rightDownDir = solveRecursion(grid, r1, c1+1, c2, dp);
+        int rightRightDir = solveRecursion(grid, r1, c1+1, c2+1, dp);
         
-        return dp[r1][c1][c2] = maxCherriesPicked + Math.max(downdown, Math.max(downright, Math.max(rightdown, rightright)));
+        return dp[r1][c1][c2] = maxCherriesPicked + Math.max(downDownDir, Math.max(downRightDir, Math.max(rightDownDir, rightRightDir)));
     }
 }
