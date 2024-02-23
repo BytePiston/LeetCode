aae03fb2398349e5709f8c58a85971f44811fc0e
Neel Priyadarshi <neel0108p@gmail.com>
2024-02-23 16:39:28 -0700

    Time: 127 ms (11.09%), Space: 46.3 MB (5.08%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,24 +1,31 @@
 class Solution {
     public int jump(int[] nums) {
-        Map<Integer, Integer> memo = new HashMap<>();
-        return jumpHelper(nums, 0, memo);
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return jumpHelperMemoization(nums, 0, dp);
     }
     
-    int jumpHelper(int[] nums, int index, Map<Integer, Integer> memo){
+    //Iterative Approach with Tabulization
+    int jumpHelperTabulization(int[] nums, int index, Map<Integer, Integer> memo){
+        return 0;
+    }
+    
+    // Recursive Approach with Memoization
+    int jumpHelperMemoization(int[] nums, int index, int[] dp){
         if(index >= nums.length-1){
             return 0;
         }
         
-        if(memo.containsKey(index)){
-            return memo.get(index);
+        if(dp[index] != -1){
+            return dp[index];
         }
         
         int minJump = 99999; 
         
         for(int i=index+1; i<=index+nums[index] && i <nums.length; i++){
-            minJump = Math.min(minJump, jumpHelper(nums, i, memo)+1);
+            minJump = Math.min(minJump, jumpHelperMemoization(nums, i, dp)+1);
         }
-        memo.put(index, minJump);
-        return memo.get(index);    
+        
+        return dp[index] = minJump;    
     }
 }
