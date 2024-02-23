88a4fda0f6ff6488c248f2481342c242bb58a35b
Neel Priyadarshi <neel0108p@gmail.com>
2024-02-23 16:29:18 -0700

    Time: 944 ms (5.02%), Space: 49.6 MB (5.08%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int jump(int[] nums) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        return jumpHelper(nums, 0, memo);
+    }
+    
+    int jumpHelper(int[] nums, int index, Map<Integer, Integer> memo){
+        if(index >= nums.length-1){
+            return 0;
+        }
+        
+        if(memo.containsKey(index)){
+            return memo.get(index);
+        }
+        
+        int minJump = 9999999; 
+        
+        for(int i=index+1; i<=index+nums[index] && i <nums.length; i++){
+            minJump = Math.min(minJump, jumpHelper(nums, i, memo)+1);
+        }
+        memo.put(index, minJump);
+        return memo.get(index);    
+    }
+}
