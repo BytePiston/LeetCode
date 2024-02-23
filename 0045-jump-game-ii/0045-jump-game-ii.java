d33e2ce701e26ceff733a90bf1f3759faec754ff
Neel Priyadarshi <neel0108p@gmail.com>
2024-02-23 16:52:40 -0700

    Time: 33 ms (25.88%), Space: 45.6 MB (20.46%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -2,12 +2,25 @@ class Solution {
     public int jump(int[] nums) {
         int[] dp = new int[nums.length];
         Arrays.fill(dp, -1);
-        return jumpHelperMemoization(nums, 0, dp);
+        // return jumpHelperMemoization(nums, 0, dp);
+        return jumpHelperTabulization(nums, dp);
     }
     
     //Iterative Approach with Tabulization
-    int jumpHelperTabulization(int[] nums, int index, Map<Integer, Integer> memo){
-        return 0;
+    int jumpHelperTabulization(int[] nums, int[] dp){
+        for(int index=nums.length-1; index>=0; index--){
+            if(index == nums.length-1){
+                dp[index] = 0;
+                continue;
+            }
+
+            int minJump = 99999;
+            for(int i=index+1; i<=index+nums[index] && i <nums.length; i++){
+                minJump = Math.min(minJump, dp[i]+1);
+            }
+            dp[index] = minJump;
+        }
+        return dp[0];
     }
     
     // Recursive Approach with Memoization
