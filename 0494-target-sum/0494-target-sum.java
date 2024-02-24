c69924edc6e88538b990a2e299702e4c03059aa3
Neel Priyadarshi <11568281+BytePiston@users.noreply.github.com>
2024-02-23 22:44:39 -0700

    Time: 110 ms (46.08%), Space: 45.2 MB (18.00%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,20 +1,26 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return findTargetSumWaysHelper(nums, target, 0, 0);
+        Map<String, Integer> memo = new HashMap<>();
+        return findTargetSumWaysHelper(nums, target, 0, 0, memo);
     }
     
-    int findTargetSumWaysHelper(int[] nums, int target, int index, int sum){
+    //Recursive solution with Memoization;
+    int findTargetSumWaysHelper(int[] nums, int target, int index, int sum, Map<String, Integer> memo){
         
         if(target == sum && index == nums.length){
             return 1;
         } else if(index > nums.length-1){
             return 0;
         }
+        String key = sum +":"+index;
+        if(memo.containsKey(key)){
+            return memo.get(key);
+        }
         
         int result = 0;
-        result += findTargetSumWaysHelper(nums, target, index+1, sum-nums[index]);
-        result += findTargetSumWaysHelper(nums, target, index+1, sum+nums[index]);
-    
-        return result;   
+        result += findTargetSumWaysHelper(nums, target, index+1, sum-nums[index], memo);
+        result += findTargetSumWaysHelper(nums, target, index+1, sum+nums[index], memo);
+        memo.put(key, result);
+        return memo.get(key);   
     }
 }
