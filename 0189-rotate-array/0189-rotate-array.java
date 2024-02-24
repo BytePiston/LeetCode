f9700a647ebe1b51c36890f8d31edb066237ad61
Neel Priyadarshi <11568281+BytePiston@users.noreply.github.com>
2024-02-24 12:10:55 -0700

    Time: 1 ms (53.50%), Space: 57.6 MB (16.51%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,11 +1,11 @@
 class Solution {
     
-    // Native Approch with K rotation; Time Complexity: O(N) with Extra space
-    public void rotate(int[] nums, int k) {
-        int n  = nums.length;
-        if(k>n){
-            k = k%n;
-        }
+    // Native Approch with K rotation; Time Complexity: O(N); Space Complexity: O(N)
+    public void rotateNative(int[] nums, int k) {
+        
+        int n = nums.length;
+        k = k%n;
+        
         if(n>1){
             Deque<Integer> deque = new LinkedList<>();
             for(int i=0; i<k; i++){
@@ -17,6 +17,25 @@ class Solution {
             for(int i=0; i<n; i++){
                 nums[i] = deque.removeFirst();
             }
+        }
+    }
+
+    // Optimized Approch; Time Complexity: O(N); Space Complexity: O(1) (In-place Swapping)
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k%n;
+        reverseArray(nums, n-k, n-1);
+        reverseArray(nums, 0, n-k-1);
+        reverseArray(nums, 0, n-1);
+    }
+    
+    void reverseArray(int[] nums, int start, int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
