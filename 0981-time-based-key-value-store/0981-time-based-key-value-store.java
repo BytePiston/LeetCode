72b9eb4084a3c5697bf5c62901c92ca6a30e1e79
Neel Priyadarshi <11568281+BytePiston@users.noreply.github.com>
2024-02-23 20:07:24 -0700

    Time: 136 ms (80.03%), Space: 118.6 MB (72.38%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,3 +1,59 @@
+//Using InnerClass and Binary Search Approach;
+class TimeMap {
+    
+    class Pair {
+        int timestamp;
+        String value;
+        
+        Pair(int timestamp, String value){
+            this.timestamp = timestamp;
+            this.value = value;
+        }
+    }
+    
+    Map<String, List<Pair>> map;
+    
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if(!map.containsKey(key)){
+            map.put(key, new ArrayList<>());
+        }
+        map.get(key).add(new Pair(timestamp, value));
+    }
+    
+    public String get(String key, int timestamp) {
+        if(!map.containsKey(key)){
+            return "";
+        }
+        int result = search(map.get(key), timestamp);
+        return result == -1 ? "" : map.get(key).get(result).value;
+    }
+    
+    int search(List<Pair> pairList, int target){
+        int index = -1;
+        int start = 0;
+        int end = pairList.size()-1;
+        int mid = 0;
+        while(start <= end){
+            mid = start + (end-start)/2;
+            
+            if(pairList.get(mid).timestamp <= target){
+                index = mid;
+                start = mid+1;
+            } else{
+                end = mid - 1;
+            }
+        }
+        return index;
+    }
+}
+
+
+/*
+//Using TreeMap Approach;
 class TimeMap {
     
     Map<String, TreeMap<Integer, String>> map = new HashMap<>();
@@ -28,6 +84,7 @@ class TimeMap {
         return treemap.get(floorNode);
     }
 }
+*/
 
 /**
  * Your TimeMap object will be instantiated and called as such:
