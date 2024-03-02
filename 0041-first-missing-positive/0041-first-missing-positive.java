adc041aaa05baac40bd3d8dc2012fec91271494b
Neel Priyadarshi <neel0108p@gmail.com>
2024-03-02 01:11:54 -0700

    Time: 2 ms (43.58%), Space: 54.7 MB (92.51%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,25 +1,49 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        // return firstMissingUsingSet(nums);
+        return firstMissingUsingCyclicSort(nums);
+    }
+    
+    //Using cyclic sort without extra space;
+    int firstMissingUsingCyclicSort(int[] nums){
         cyclicSort(nums);
-        for(int i=0; i<nums.length; i++)
-            if(nums[i] != i+1)
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != i+1){
                 return i+1;
+            }
+        }
         return nums.length+1;
     }
-
+    
     void cyclicSort(int[] nums){
-        int i = 0;
+        int i=0;
         while(i<nums.length){
-            if(nums[i]>0 && nums[i]<nums.length && nums[i] != nums[nums[i]-1])
+            if(nums[i]>0 && nums[i]<nums.length && nums[i] != nums[nums[i]-1]){
                 swap(nums, i, nums[i]-1);
-            else
-                i++;
+            } else{
+                i++;   
+            }
         }
     }
-
+    
     void swap(int[] nums, int first, int second){
         int temp = nums[first];
         nums[first] = nums[second];
-        nums[second] = temp;
+        nums[second] = temp;   
+    }
+    
+    //Using extra space;
+    int firstMissingUsingSet(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums){
+            set.add(num);
+        }
+        
+        for(int i=1; i<=nums.length; i++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+        return nums.length+1;
     }
 }
