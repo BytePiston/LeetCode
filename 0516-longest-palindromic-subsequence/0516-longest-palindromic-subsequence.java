ba4f401b2409307f1b5923e742b7e5daac91d350
Neel Priyadarshi <neel0108p@gmail.com>
2024-03-04 02:00:35 -0700

    Time: 37 ms (68.83%), Space: 54.9 MB (70.68%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,29 +1,25 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        String rev = reverse(s);
-        int[][] dp = new int[s.length()][s.length()];
-        for(int i=0; i<dp.length; i++)
-            for(int j=0; j<dp.length; j++)
-                dp[i][j] = -1;
-        return lcs(s, s.length()-1, rev, rev.length()-1, dp);
-    }
-
-    String reverse(String s){
-        StringBuilder sb = new StringBuilder(s);  
-        sb.reverse();  
-        return sb.toString();  
-    }
-
-    int lcs(String s1, int index1, String s2, int index2, int[][] dp){
-        if(index1 < 0 || index2 < 0)
-            return 0;
-        if(dp[index1][index2] != -1)
-            return dp[index1][index2];
-        if(s1.charAt(index1) == s2.charAt(index2)){
-            dp[index1][index2] = 1 + lcs(s1, index1-1, s2, index2-1, dp);
-            return dp[index1][index2];
+        String reversedStr = new StringBuilder(s).reverse().toString();
+        int[][] memo = new int[s.length()][s.length()];
+        for(int[] row: memo){
+            Arrays.fill(row, -1);
         }
-        dp[index1][index2] = 0 + Math.max(lcs(s1, index1-1, s2, index2, dp), lcs(s1, index1, s2, index2-1, dp));
-        return dp[index1][index2];
+        return lpsHelper(s.toCharArray(), s.length()-1, reversedStr.toCharArray(), reversedStr.length()-1, memo);
+    }
+    
+    int lpsHelper(char[] charArr1, int index1, char[] charArr2, int index2, int[][] memo){
+        if(index1 < 0 || index2 < 0){
+            return 0;
+        }
+        
+        if(memo[index1][index2] != -1){
+            return memo[index1][index2];
+        }
+        
+        if(charArr1[index1] == charArr2[index2]){
+            return memo[index1][index2] = 1 + lpsHelper(charArr1, index1-1, charArr2, index2-1, memo);
+        }
+        return memo[index1][index2] = 0 + Math.max(lpsHelper(charArr1, index1-1, charArr2, index2, memo), lpsHelper(charArr1, index1, charArr2, index2-1, memo));
     }
 }
